a5fdf8b28bbd4307c08dd553cc864453ee963e55
Elena Kuchueva <kuchuevaelena@yandex.ru>
2023-03-04 23:26:45 +0300

    открыть попап с картинкой

--- scripts/index.js
+++ scripts/index.js
@@ -74,30 +74,29 @@ const сards = [
 //шаблон создания корточки
 const elements = document.querySelector('.elements');
 
-const createCard = function (card) {
+сards.forEach(function(card){
   const newCard = document.querySelector('#card').content.cloneNode(true);
   const cardHeading = newCard.querySelector('.element__title');
   cardHeading.textContent = card.name;
   const cardImage = newCard.querySelector('.element__image');
-  cardImage.setAttribute('src',card.link);
+  cardImage.setAttribute('src', card.link);
   elements.prepend(newCard);
-  //deleteCard;
   like();
-  //openFullImage;
-  //return newCard;
-};
-сards.forEach(createCard);
+  deleteCard();
+});
 
 //удаление карточки
-const delate = document.querySelector('.element__delate');
-const deleteCard = delate.addEventListener('click', () => {
+function deleteCard() {
+  const delate = document.querySelector('.element__delate');
+  delate.addEventListener('click', () => {
   const delateElement = delate.closest ('.element');
   delateElement.remove();
 });
+};
 
 //лайк
-const icon = document.querySelector('.element__icon');
 function like(){
+  const icon = document.querySelector('.element__icon');
   icon.addEventListener('click', (evt) => {
   const eventTarget = evt.target;
   eventTarget.classList.toggle("element__icon_active");
@@ -112,18 +111,20 @@ AddProfileButton.addEventListener ('click', ()=> {
 //добавить новую карточку
 //function handleFormEddSubmit (evt) {
 //evt.preventDefault();
+//place.value = 
+//link.value = 
 //createCard();
 //close();
 //};
 //formElement.addEventListener('submit', handleFormEddSubmit);
 
 //поп-ап - открыть картинку карточки
-//function openPopupImage () {
-//popupImage.classList.add('popup-image_opened');
-//popupImageCard.src = 
-//popupImageTitle.textContent = 
-//};
-//const openFullImage = popupImageCard.addEventListener('click', openPopupImage());
+function openPopupImage () {
+  popupImage.classList.add('popup-image_opened');
+ // popupImageCard.src = cards.link;
+  //popupImageTitle.textContent = cards.name;
+};
+const openFullImage = popupImageCard.addEventListener('click', openPopupImage());
 
 //const renderCard = ({name,link}) => {
 //createCard;
